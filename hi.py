eeb5cad47e4d4ff0544513dfba92d322a1f8673a
craig rickle <csrickle@gmail.com>
2025-05-04 21:42:06 -0400

    added 2nd test

--- hi.py
+++ hi.py
@@ -1,7 +1,14 @@
 import platform
 
-def test_dummy():
-    pass
+
+def inc(x):
+    return x + 1
 
 print("hi")
 print(platform.python_version())
+
+def test_answer():
+    assert inc(3) == 4
+    
+def test_dummy():
+    pass
