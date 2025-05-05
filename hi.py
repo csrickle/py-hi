46e4eeb321e2c5200b5c828e59a1ab1aff260086
craig rickle <csrickle@gmail.com>
2025-05-04 22:16:03 -0400

    test example

--- hi.py
+++ hi.py
@@ -1,16 +1,16 @@
-import platform
-
-print("hi")
-print(platform.python_version())
-   
-def test_dummy():
-    pass
+# import platform
+# 
+# print("hi")
+# print(platform.python_version())
+#    
+# def test_dummy():
+#     pass
 
 # content of test_sample.py
-# def inc(x):
-#     return x + 1
-# 
-# 
-# def test_answer():
-#     assert inc(3) == 4
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 4
 
