46208b7cec5919d1e7908eb9eb2d93a353b3faa9
craig rickle <csrickle@gmail.com>
2025-05-04 22:02:20 -0400

    pytest local works

--- hi.py
+++ hi.py
@@ -1,14 +1,16 @@
 import platform
 
-
-def inc(x):
-    return x + 1
-
 print("hi")
 print(platform.python_version())
-
-def test_answer():
-    assert inc(3) == 4
-    
+   
 def test_dummy():
     pass
+
+# content of test_sample.py
+# def inc(x):
+#     return x + 1
+# 
+# 
+# def test_answer():
+#     assert inc(3) == 4
+
